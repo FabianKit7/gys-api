46f7c6d8da2d3cf6d0c7710a3a52d627c2dceea6
innocentdavid <paulinnocent04@gmail.com>
2024-06-03 06:57:54 +0100

    updated

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import stripeRoutes, { sendSMS } from "./routes/stripeRoutes.js";
 import bodyParser from "body-parser";
 import axios from "axios";
 import Slack from "@slack/bolt";
+import cron from "node-cron";
+import { createClient } from "@supabase/supabase-js";
 
 dotenv.config({ path: ".env" });
 const PORT = process.env.PORT || 8000;
@@ -13,6 +15,15 @@ const app = express();
 // app.use(express.urlencoded())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseAdmin = createClient(supabaseUrl, supabaseKey);
+
+const SCRAPER_API_URL =
+  "https://instagram-bulk-profile-scrapper.p.rapidapi.com/clients/api/ig/ig_profile";
+const X_RAPID_API_HOST = "instagram-bulk-profile-scrapper.p.rapidapi.com";
+const X_RAPID_API_KEY = process.env.X_RAPID_API_KEY;
 
 const send_email = (to, subject, content) => {
   if (!to || !subject || !content) {
@@ -75,6 +86,217 @@ const send_email = (to, subject, content) => {
 //   signingSecret: process.env.SLACK_SIGNING_SECRET,
 //   token: process.env.SLACK_BOT_TOKEN,
 // });
+
+const updateUsersGraphDataCron = async () => {
+  console.log("\n\nupdateUsersGraphData cron job started...");
+  try {
+    // Fetch users from the database
+    const { data: users, error } = await fetchUsers();
+    if (error) {
+      console.error("Failed to get users:", error.message);
+      return;
+    }
+
+    const filteredData = users.filter((user) => needsGraphData(user));
+
+    console.log(`Users that need Dummy data: ${filteredData.length}`);
+
+    // Process each user in batches to avoid rate limiting
+    await processUsersInBatches(filteredData, dummyGraphGenerator);
+  } catch (error) {
+    console.error("Error in updateUsersGraphDataCron:", error.message);
+  }
+  console.log("updateUsersGraphData cron job done\n\n");
+};
+
+// Fetch users from the database
+const fetchUsers = async () => {
+  const { data, error } = await supabaseAdmin
+    .from("users")
+    .select("id, username, dummyData, status, created_at")
+    .in("status", ["active", "checking", "new"])
+    .order("created_at", { ascending: false });
+
+  return { data, error };
+};
+
+// Check if a user needs dummy data
+const needsGraphData = (user) => {
+  if (user.dummyData.length === 0) return true;
+  const lastDataPointDate = new Date(
+    user.dummyData[user.dummyData.length - 1].start_time
+  );
+  const currentDate = new Date();
+  return !(
+    lastDataPointDate.getFullYear() === currentDate.getFullYear() &&
+    lastDataPointDate.getMonth() === currentDate.getMonth() &&
+    lastDataPointDate.getDate() === currentDate.getDate()
+  );
+};
+
+// Process users in batches to avoid rate limiting
+const processUsersInBatches = async (users, callback) => {
+  const requestsPerMinuteLimit = 25;
+  let apiRequestCounter = 0;
+  let retries = 0;
+  const maxRetries = 30;
+  // const retryDelay = 30000; // 30 seconds
+  const doneList = [];
+  let lastRequestTime = Date.now();
+
+  while (retries < maxRetries && doneList.length !== users.length) {
+    console.log({ retries, apiRequestCounter });
+
+    const currentTime = Date.now();
+    const timeSinceLastRequest = currentTime - lastRequestTime;
+    console.log({ timeSinceLastRequest });
+
+    // If 25 API calls have been made and less than a minute has passed since the first of those calls, wait.
+    // if (apiRequestCounter >= requestsPerMinuteLimit) {
+    if (
+      timeSinceLastRequest > 60000 &&
+      apiRequestCounter >= requestsPerMinuteLimit
+    ) {
+      const waitTime = 60000 - timeSinceLastRequest;
+      console.log(
+        `waiting for ${waitTime / 1000} seconds before continuing...`
+      );
+      await new Promise((resolve) => setTimeout(resolve, waitTime));
+      apiRequestCounter = 0;
+      lastRequestTime = Date.now();
+    }
+
+    for (const user of users) {
+      if (doneList.find((u) => u.username === user.username)) continue;
+      console.log(`Processing ${user.username}`);
+      await callback(user);
+      console.log(`\n\n`);
+      apiRequestCounter++;
+      doneList.push(user);
+
+      console.log("doneList.length");
+      console.log(doneList.length);
+
+      if (
+        timeSinceLastRequest > 60000 &&
+        apiRequestCounter >= requestsPerMinuteLimit
+      )
+        break;
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    retries++;
+  }
+
+  if (doneList.length === users.length) {
+    console.log("all done");
+  }
+
+  if (users.length === 0) {
+    console.log("0 users found");
+  }
+};
+
+// Generate dummy graph data for a user
+const dummyGraphGenerator = async (user) => {
+  try {
+    const params = {
+      ig: user.username,
+      response_type: "short",
+      corsEnabled: "false",
+      storageEnabled: "true",
+    };
+    const options = {
+      method: "GET",
+      url: SCRAPER_API_URL,
+      params,
+      headers: {
+        "X-RapidAPI-Key": X_RAPID_API_KEY,
+        "X-RapidAPI-Host": X_RAPID_API_HOST,
+      },
+    };
+
+    const start_get_vuser = Date.now();
+    const userResults = await axios.request(options);
+    console.log(
+      `get vuser took: ${(Date.now() - start_get_vuser) / 1000}`
+    );
+    const vuser = userResults.data?.[0];
+
+    if (vuser) {
+      await updateUserGraphData(user, vuser);
+    } else {
+      console.error("No valid user data found for", user.username);
+    }
+  } catch (error) {
+    console.error(`Error processing ${user.username}:`, error.message);
+  }
+};
+
+// Update user dummy data in the database
+const updateUserGraphData = async (user, vuser) => {
+  const currentDate = new Date();
+  const formattedDate = `${currentDate.getFullYear()}-${(
+    currentDate.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, "0")}-${currentDate
+    .getDate()
+    .toString()
+    .padStart(2, "0")} ${currentDate
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${currentDate
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}:${currentDate
+    .getSeconds()
+    .toString()
+    .padStart(2, "0")}.${currentDate
+    .getMilliseconds()
+    .toString()
+    .padStart(6, "0")}`;
+
+  const dataPoint = {
+    profile: {
+      followers: vuser.follower_count,
+      following: vuser.following_count,
+      total_interactions: 0,
+    },
+    start_time: formattedDate,
+  };
+
+  console.log("graphData for: " + user.username);
+  console.log(dataPoint);
+
+  // console.log(`start supabase update: ${Date.now()}`);
+  const start_supabase_update = Date.now();
+  const { error } = await supabaseAdmin
+    .from("users")
+    .update({
+      dummyData: [...(user.dummyData || []), dataPoint],
+    })
+    .eq("id", user.id); // 0.3s average
+  console.log(
+    `supabase update took: ${(start_supabase_update - Date.now()) / 1000}`
+  );
+
+  if (error) {
+    console.error(
+      `Failed to update ${user.username}'s graph data:`,
+      error.message
+    );
+  } else {
+    console.log(`${user.username} updated successfully`);
+  }
+};
+
+// updateUsersGraphDataCron();
+
+// Schedule the cron job to run every day at 7am
+cron.schedule("0 6 * * *", updateUsersGraphDataCron);
+
+console.log("Scheduled the cron job to run every day at 7am (0 7 * * *)");
 
 app.post("/api/slack-notify", async (req, res) => {
   const { username, cancellation } = req.body;
